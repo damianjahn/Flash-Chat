812874b58ab14cadfe350ece408b6010fb12dbca
Damian <damian.jahn@icloud.com>
2020-05-15 10:49:09 +0200

    Typing Animations

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -15,6 +15,15 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabel.text = ""
+        var characterIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for lettet in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(lettet)
+            }
+            characterIndex += 1
+        }
        
     }
     
